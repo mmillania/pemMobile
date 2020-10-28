3689adbe5a6eab58bd85d75fbbccfddff54031b0
mmillania <mmillania09@gmail.com>
2020-10-28 18:12:45 +0700

    Tugas modul 4.1

---
+++ tugas_Modul 41/DateTimePickers/app/src/main/java/org/aplas/datetimepickers/DatePickerFragment.java
@@ -0,0 +1,39 @@
+package org.aplas.datetimepickers;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it.
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Convert the date elements to strings.
+        // Set the activity to the Main Activity.
+        MainActivity activity = (MainActivity) getActivity();
+        // Invoke Main Activity's processDatePickerResult() method.
+        activity.processDatePickerResult(year, month, day);
+    }
+
+}
